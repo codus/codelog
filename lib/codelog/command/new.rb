ac09a1c9f82b426470c456ad14e1a53f49370601
Rodrigo Masaru Ohashi <rodrigo.ohashi@codus.com.br>
2018-01-10 17:46:40 -0200

    add files for the change file generator

---
+++ lib/codelog/command/new.rb
@@ -0,0 +1,30 @@
+require 'fileutils'
+
+module Codelog
+  module Command
+    class New
+      include FileUtils
+
+      def self.run
+        Codelog::Command::New.new.run
+      end
+
+      def run
+        chdir Dir.pwd do
+          # This script create a change file for the changelog documentation.
+
+          full_file_name = "changelogs/unreleased/#{Time.now.to_i}_change.yml"
+
+          puts "== Creating #{full_file_name} change file based on example =="
+          system! "cp changelogs/template.yml #{full_file_name}"
+        end
+      end
+
+      private
+
+      def system!(*args)
+        system(*args) || abort("\n== Command #{args} failed ==")
+      end
+    end
+  end
+end
---
+++ spec/codelog/command/new_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Codelog::Command::New do
+  describe '#run' do
+    before :each do
+      allow(Time).to receive_message_chain(:now, :to_i) { '1234' }
+      allow(subject).to receive(:puts)
+      allow(subject).to receive(:system) { true }
+      subject.run
+    end
+
+    it 'prints a message to notify the user about the file creation' do
+      expect(subject).to have_received(:puts)
+        .with('== Creating changelogs/unreleased/1234_change.yml change file based on example ==')
+    end
+
+    it 'creates a file for the unreleased partial changes' do
+      expect(subject).to have_received(:system)
+        .with('cp changelogs/template.yml changelogs/unreleased/1234_change.yml')
+    end
+  end
+
+  describe '.run' do
+    it 'creates an instance of the class to run the command' do
+      expect_any_instance_of(described_class).to receive(:run)
+      described_class.run
+    end
+  end
+end
