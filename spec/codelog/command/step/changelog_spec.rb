9b4abc12fa5e3c4de39fd1f62af02d4234968ad4
Rodrigo Masaru Ohashi <rodrigo.ohashi@codus.com.br>
2018-01-12 11:10:11 -0200

    add the changelog generation step

---
+++ spec/codelog/command/step/changelog_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Codelog::Command::Step::Changelog do
+  let(:mocked_changelog) { double(File) }
+
+  describe '#run' do
+    before :each do
+      allow(Dir).to receive(:'[]').with('changelogs/releases/*.md') { ['0.1.0.md', '0.2.0.md'] }
+      allow(File).to receive(:readlines).with('0.1.0.md') { ['line_1\n'] }
+      allow(File).to receive(:readlines).with('0.2.0.md') { ['line_2\n'] }
+    end
+
+    it 'combines the content of the releases and put in an array' do
+      expect(subject).to receive(:create_file_from)
+        .with(['line_2\n', 'line_1\n'])
+      subject.run
+    end
+
+    it 'creates a changelog file from the releases' do
+      allow(mocked_changelog).to receive(:puts)
+      expect(File).to receive(:open).with('CHANGELOG.md', 'w+').and_yield mocked_changelog
+      subject.run
+      expect(mocked_changelog).to have_received(:puts).with(['line_2\n', 'line_1\n'])
+    end
+  end
+
+  describe '.run' do
+    it 'creates an instance of the class to run the command' do
+      expect_any_instance_of(described_class).to receive(:run)
+      described_class.run
+    end
+  end
+end
