2e91c8d6a7bd5bd1df0aa1f88d7ddc0c85ace407
Felipe Michelon <felipe.michelon@codus.com.br>
2018-02-06 17:15:09 -0200

    fixed chagelog spec

--- spec/codelog/command/step/changelog_spec.rb
+++ spec/codelog/command/step/changelog_spec.rb
@@ -17,18 +17,29 @@ describe Codelog::Command::Step::Changelog do
       allow(YAML).to receive(:load_file).with('0.2.0.yml') { { 'Version' => '0.2.0', 'Date' => stubed_date } }
     end
 
-    it 'creates CHANGELOG file' do
-      expect(subject).to receive(:create_file)
-      subject.run
+    context 'without a changelog-backup file' do
+      before :each do
+        allow(File).to receive(:file?).with('changelogs/releases/changelog-backup.md').and_return(false)
+      end
+
+      it 'creates CHANGELOG file' do
+        expect(subject).to receive(:create_file)
+        subject.run
+      end
+
+      it 'creates a changelog file from the releases' do
+        allow(mocked_changelog).to receive(:puts)
+        expect(File).to receive(:open).with('CHANGELOG.md', 'w+').and_yield mocked_changelog
+        subject.run
+      end
     end
 
-    it 'creates a changelog file from the releases' do
-      allow(mocked_changelog).to receive(:puts)
-      expect(File).to receive(:open).with('CHANGELOG.md', 'w+').and_yield mocked_changelog
-      subject.run
+    context 'with a changelog-backup file' do
+      it 'includes changelog-backup if it exists' do
+        pending('to do')
+      end
     end
   end
-
   describe '.run' do
     it 'creates an instance of the class to run the command' do
       expect_any_instance_of(described_class).to receive(:run)
