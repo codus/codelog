5498f3b614acf6d6801ea78d535a4cc3a52c149b
Rodrigo Masaru Ohashi <ohashi.masaru@gmail.com>
2018-03-01 00:02:05 -0300

    Set command to receive the change file name
    The new command will now receive a name parameter that will be converted to snake case and added to the change file name

--- lib/codelog/command/new.rb
+++ lib/codelog/command/new.rb
@@ -5,19 +5,24 @@ module Codelog
     class New
       include FileUtils
 
-      def self.run(options)
-        Codelog::Command::New.new.run options
+      def initialize(name, options)
+        @name = name
+        @options = options
       end
 
-      def run(options)
+      def self.run(name, options)
+        Codelog::Command::New.new(name, options).run
+      end
+
+      def run
         chdir Dir.pwd do
           # This script create a change file for the changelog documentation.
 
-          full_file_name = "changelogs/unreleased/#{Time.now.strftime('%Y%m%d%H%M%S%L')}_change.yml"
+          full_file_name = "changelogs/unreleased/#{change_file_timestamp}_#{change_file_name}.yml"
 
           puts "== Creating #{full_file_name} change file based on example =="
           system! "cp changelogs/template.yml #{full_file_name}"
-          system! "#{default_editor} #{full_file_name}" if options[:edit]
+          system! "#{default_editor} #{full_file_name}" if @options[:edit]
         end
       end
 
@@ -28,6 +33,14 @@ module Codelog
         # if no variable is set it defaults to nano
 
         '${VISUAL:-${EDITOR:-nano}}'
+      end
+
+      def change_file_timestamp
+        Time.now.strftime('%Y%m%d%H%M%S%L')
+      end
+
+      def change_file_name
+        @name.gsub(/(.)([A-Z])/, '\1_\2').downcase
       end
 
       def system!(*args)
