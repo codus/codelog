56d4c1329f4d8e43db94bb21ed2650c0ac557189
Guilherme Moreira Santos <gmoreirasantos1325@hotmail.com>
2018-05-23 08:51:33 -0300

    Disable rubocop guard clause cop

--- lib/codelog/command/release.rb
+++ lib/codelog/command/release.rb
@@ -7,9 +7,11 @@ module Codelog
 
       def run(version_number, release_date, options)
         Codelog::Command::Step::Version.run version_number, release_date, options
-        Codelog::Command::Step::Delete.run unless options[:preview]
-        Codelog::Command::Step::Changelog.run unless options[:preview]
-        puts "\n== Changelog updated to version #{version_number} ==" unless options[:preview]
+        unless options[:preview]
+          Codelog::Command::Step::Delete.run
+          Codelog::Command::Step::Changelog.run
+          puts "\n== Changelog updated to version #{version_number} =="
+        end
       end
     end
   end
