6fd8fdc6f31df47b8db4076c1139a79da08d5fc6
Joey Paris <joey@leadjig.com>
2021-04-16 11:21:44 -0400

    Add additional regenerate rspec test

--- spec/codelog/command/regenerate_spec.rb
+++ spec/codelog/command/regenerate_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe Codelog::Command::Regenerate do
+  describe '#run' do
+    it 'calls the "Changelog" step to re-generate the CHANGELOG using existing releases' do
+      expect(Codelog::Command::Step::Changelog).to receive(:run)
+      expect(subject).to receive(:puts).with("The CHANGELOG was regenerated successfully!")
+      subject.run
+    end
+  end
+
   describe '.run' do
     it 'calls the "Changelog" step to re-generate the CHANGELOG using existing releases' do
       allow(Codelog::Command::Step::Changelog).to receive(:run)
-
       described_class.run
-
       expect(Codelog::Command::Step::Changelog).to have_received(:run)
     end
   end
