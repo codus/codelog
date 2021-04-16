c09b76da57de33a4cafec6acc6254c3439e501d0
Joey Paris <joey@leadjig.com>
2021-04-16 11:14:16 -0400

    Fix rubocop offenses

--- lib/codelog/command/pending.rb
+++ lib/codelog/command/pending.rb
@@ -9,7 +9,9 @@ module Codelog
 
       def run(title)
         outputter = Codelog::Output::ReleaseFile.new("#{RELEASES_PATH}/pending.md")
-        Codelog::Command::Step::Version.run title, Date.today.strftime(Codelog::Config.date_input_format), outputter
+        Codelog::Command::Step::Version.run title,
+                                            Date.today.strftime(Codelog::Config.date_input_format),
+                                            outputter
         Codelog::Command::Step::Changelog.run
         puts "\n== Pending changes added to changelog =="
       end
